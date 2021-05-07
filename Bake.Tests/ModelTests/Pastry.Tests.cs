fe88fe1c3dd25538812bde2bc3c1cd4948b97140
yesthecarlos <carlosmendez86@gmail.com>
2021-05-07 09:40:23 -0700

    add test files

---
+++ Bake.Tests/ModelTests/Pastry.Tests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Bake.Models;
+
+namespace Bake.Test
+{
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void 
+    //Arrange
+    //Act
+    //Assert
+  }
+}
